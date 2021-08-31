13ee4e9bcc5ed26afe6277856533413a53226372
José Luis Aguilar Nucamendi <luisaguilarnucamendi@gmail.com>
2021-08-31 17:31:36 +0000

    adding argument validation

--- labs/calculator/calculator.c
+++ labs/calculator/calculator.c
@@ -1,51 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 // adds/subtracts/multiplies all values that are in the *values array.
 // nValues is the number of values you're reading from the array
 // operator will indicate if it's an addition (1), subtraction (2) or
 // multiplication (3)
+
 long calc(int operator, int nValues, int *values) {
-    if(nValues > 1){
-        int resultado = values[2];
-        printf("%i", values[2]);
-        for (int i = 3; i < nValues; i++){
-            if(operator == 1){
-                printf(" + %i", values[i]);
-                resultado += values[i];
-            }else if(operator == 2){
-                printf(" - %i", values[i]);
-                resultado -= values[i];
-            }else if(operator == 3){
-                 printf(" * %i", values[i]);
-                resultado *= values[i];
-            }
-        } 
-        printf(" = %i ", resultado);  
-        printf("\n");  
-    }
+    
+    int resultado = values[2];
+    printf("%i", resultado);
+    for (int i = 3; i < nValues; i++){
+        if(operator == 1){
+            printf(" + %i", values[i]);
+            resultado += values[i];
+        }else if(operator == 2){
+            printf(" - %i", values[i]);
+            resultado -= values[i];
+        }else{
+            printf(" * %i", values[i]);
+            resultado *= values[i];
+        }
+    } 
+    printf(" = %i \n", resultado);  
+   
     return 0;
 }
 
 int main(int argc, char*argv[]) {
-    int num[argc];
-    for (int i = 0; i < argc; i++){
-        num[i]=atoi(argv[i]);
-    }
-    
-    int operator = 0;
-    if(strcmp("add", argv[1]) == 0){
-        operator = 1;
-    }
 
-    if(strcmp("sub", argv[1]) == 0){
-        operator = 2;     
+    if(argc > 3){
+        int operator = 0;
+
+        if(strcmp("add", argv[1]) == 0){
+            operator = 1;
+        }else if(strcmp("sub", argv[1]) == 0){
+            operator = 2;     
+        }else if(strcmp("mult", argv[1]) == 0){
+            operator = 3;
+        }else{
+            printf("El operador %s no es valido \n", argv[1]);
+            return -1;
+        }
+
+        int num[argc];     
+
+        for (int i = 2; i < argc; ++i) {
+            for(int j=0; j < strlen(argv[i]); j++){
+                char letter = argv[i][j];
+                if(!isdigit(argv[i][j])){
+                    printf("No ingresaste un numero entero \n");
+                    printf("No ingresaste suficientes parametros con el formato:");
+                    printf("<operador> <numeros separados por espacio>\n");
+                    return -1;
+                } 
+            }
+            num[i]=atoi(argv[i]);   
+        }
+        
+        calc(operator,argc, num);
+        return -1;
+    }else{
+        printf("No ingresaste suficientes parametros: ");
+        printf("<operador> <numeros separados por espacio>\n");
     }
-    if(strcmp("mult", argv[1]) == 0){
-        operator = 3;
-    }
-    
-    calc(operator,argc, num);
-    return -1;
 }
