fe7099b0b10c95553e07316093351c9eaa6077c3
José Luis Aguilar Nucamendi <luisaguilarnucamendi@gmail.com>
2021-09-28 16:51:36 +0000

    solve c-str-lib

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+extern int mystrlen(char *);
+extern char *mystradd(char *, char *);
+extern int mystrfind(char *, char *);
+
+int main(int argc, char *argv[]) {
+
+    if (strcmp(argv[1], "-add") == 0 && argc == 4) {
+
+        printf("Initial Lenght      : %d\n", mystrlen(argv[2]));
+        char *newString  = mystradd(argv[2],argv[3]);
+        printf("New String          : %s\n", newString );
+        printf("New length          : %d\n", mystrlen(newString));
+        return 0;
+    }
+
+    if (strcmp(argv[1], "-find") == 0 && argc == 4) {
+        int pos = mystrfind(argv[2], argv[3]);
+        if (pos > -1) {
+            printf("['%s'] string was found at [%d] position\n", argv[3], pos);
+           
+        } else {
+            printf("String was not found");
+        }
+        return 0;
+    }
+
+    printf("Missing arguments or bad inputs\n");
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,37 @@
-
-
 int mystrlen(char *str){
-    return 0;
+    int len=0;
+    char *c;
+
+    for(c=str;*c != '\0';c++){
+        len++;
+    }
+    
+    return len;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char *result = origin + mystrlen(origin);
+    while (*addition != '\0'){
+        *result++ = *addition++;
+    }
+
+    *result = '\0';
+    return origin;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int len = 0, len2 = 0, verification = mystrlen(substr), tmp = 0;
+
+    while(origin[len] != '\0') {
+        tmp = len;
+        while(origin[len] == substr[len2] && origin[len] != '\0') {
+            len2++;
+            len++;
+        }
+        if(len2 == verification){return tmp;}
+        len2 = 0;
+        len++;   
+    }
+
+    return -1;
 }
